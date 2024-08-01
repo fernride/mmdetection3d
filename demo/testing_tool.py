1f676ce203f1c907103aa4f0957ae51d0806d148
Oleg Muratov <oleg.muratov@fernride.com>
2024-08-01 15:54:13 +0200

    cleanup testing tool

--- demo/testing_tool.py
+++ demo/testing_tool.py
@@ -275,8 +275,8 @@ class Det3dViz:
 
 
 def cli():
-    weights = Path("/home/omuratov/workspace/fern_ml/output/fernnet/fernnet_500")/"epoch_500.pth"
-    model = Path("/home/omuratov/workspace/fern_ml/output/fernnet/fernnet_500")/"pointpillars_fern3d_dynamic.py"
+    weights = Path("/home/omuratov/workspace/fern_ml/output/fernnet/fernnet_ped_500")/"epoch_500.pth"
+    model = Path("/home/omuratov/workspace/fern_ml/output/fernnet/fernnet_ped_500")/"pointpillars_fern3d_dynamic.py"
     inferencer = CustomModelRunner(model=str(model), weights=str(weights), device='cuda:0')
     samples_dir = Path("/media/omuratov/bigdata/datasets/fern3d_v0_tiny/points/")
     samples_dir = Path("/media/omuratov/bigdata/datasets/fern3d_b0_b3_filtered/points/")
@@ -285,20 +285,5 @@ def cli():
     app.reset_camera()
     gui.Application.instance.run()
 
-#def cli():
-#    weights = Path("/home/omuratov/workspace/fern_ml/output/fernnet/")/"epoch_500.pth"
-#    model = Path("/home/omuratov/workspace/fern_ml/output/fernnet/")/"pointpillars_fern3d_dynamic.py"
-#    inferencer = CustomModelRunner(model=str(model), weights=str(weights), device='cuda:0')
-#    class_names = ['car', 'truck', 'trailer', 'human', 'reach_stacker']
-#    for count, sample in enumerate(dataset_accessor()):
-#        result = inferencer.run_on_sample(sample)
-#        boxes = [gen_simple_cuboid_from_bbox(x) for x in result["bboxes"]]
-#        for box, label in zip(boxes, result["labels"]):
-#            box.class_name = class_names[label]
-#
-#        print(boxes)
-#        if count > 1:
-#            break
-#
 if __name__ == "__main__":
     cli()
